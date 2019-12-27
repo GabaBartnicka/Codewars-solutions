649a90406cc0bad56afc6eb0dc41f6e551bea83e
Gaba Bartnicka <bartnicka.g@gmail.com>
2019-12-27 21:44:37 +0100

    first commit

---
+++ src/main/java/gababartnicka/dev/Solution.java
@@ -0,0 +1,7 @@
+package gababartnicka.dev;
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/gababartnicka/dev/SolutionTest.java
@@ -0,0 +1,11 @@
+package gababartnicka.dev;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SolutionTest {
+    @Test
+    void test() {
+    }
+}
