f5803773a20f0c8fbb4bb465ef03a25eda4101b4
Gaba Bartnicka <bartnicka.g@gmail.com>
2020-01-07 19:05:26 +0100

    Valid Braces https://www.codewars.com/kata/5277c8a221e209d3f6000b56

--- src/main/java/gababartnicka/dev/Solution.java
+++ src/main/java/gababartnicka/dev/Solution.java
@@ -1,38 +1,99 @@
 package gababartnicka.dev;
 
-import java.util.stream.IntStream;
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.Stack;
+import java.util.function.Predicate;
 
 public class Solution {
-    public static boolean isPrime(int num) {
-        final double sqrt = Math.sqrt(num);
-        double ceil = Math.ceil(sqrt);
-        if ((ceil < 4 && ceil < num) || num/2.0 > ceil)
-            ceil++;
 
-        return num > 1 && IntStream.range(2, (int) ceil).filter(i -> num % i == 0).findAny().isEmpty();
-    }
+    static final Character openBrace = '(';
+    static final Character openBracket = '[';
+    static final Character openCurlyBrace = '{';
 
-    public static boolean isPrime2(int num) {
-        if (num <= 1)
+    static final Character closingBrace = ')';
+    static final Character closingBracket = ']';
+    static final Character closingCurlyBrace = '}';
+
+    public boolean isValid(String braces) {
+
+        Predicate<Character> isClosing = character -> character.equals(closingBrace) ||
+                character.equals(closingBracket) || character.equals(closingCurlyBrace);
+
+        final char[] bracesArray = braces.toCharArray();
+        if (bracesArray.length == 1)
             return false;
 
-        final double sqrt = Math.sqrt(num);
-        double ceil = Math.ceil(sqrt);
-        if ((ceil < 4 && ceil < num) || num/2.0 > ceil)
-            ceil++;
+        if (isClosing.test(bracesArray[0]))
+            return false;
 
-        System.out.println("ceil " + ceil);
+        var braceQueue = new ArrayList<Character>();
+        braceQueue.add(bracesArray[0]);
 
-        for (int i = 2; i < num; i++) {
-            if (num % i == 0) {
-                System.out.println("found " + i);
-                return false;
+        for (int i = 1; i < bracesArray.length; i++) {
+            final var brace = bracesArray[i];
+
+            if (isClosing.test(brace)) {
+                try {
+                    final int index = braceQueue.size() - 1;
+                    var previous = braceQueue.get(index);
+                    braceQueue.remove(index);
+                    var matchesToPreviousOpenedBrace = matchesToPreviousOpenedBrace(brace, previous);
+                    if (!matchesToPreviousOpenedBrace) {
+                        return false;
+                    }
+                } catch (Exception e) {
+                    return false;
+                }
+            } else {
+                braceQueue.add(brace);
             }
         }
-        return true;
+        return braceQueue.isEmpty();
     }
 
-    public static boolean isPrimeWtf(int num) {
-        return num > 1 && java.math.BigInteger.valueOf(num).isProbablePrime(20);
+
+    public boolean matchesToPreviousOpenedBrace(Character closing, Character previous) {
+        if (Objects.equals(closing, closingBrace) && Objects.equals(previous, openBrace))
+            return true;
+        if (Objects.equals(closing, closingBracket) && Objects.equals(previous, openBracket))
+            return true;
+        return Objects.equals(closing, closingCurlyBrace) && Objects.equals(previous, openCurlyBrace);
+    }
+
+    /**
+     * "(){}[]"   =>  True
+     * "([{}])"   =>  True
+     * "(}"       =>  False
+     * "[(])"     =>  False
+     * "[({})](]" =>  False
+     */
+
+
+    public boolean isValid2(String braces) {
+        Stack<Character> s = new Stack<>();
+        for (char c : braces.toCharArray())
+            if (s.size() > 0 && isClosing(s.peek(), c)) s.pop();
+            else s.push(c);
+        return s.size() == 0;
+    }
+
+    public boolean isClosing(char x, char c) {
+        return (x == '{' && c == '}') || (x == '(' && c == ')') || (x == '[' && c == ']');
+    }
+
+
+    public boolean isValid3(String braces) {
+        String b = braces;
+        System.out.println(braces);
+        for(int i=0;i<braces.length()/2;i++)
+        {
+            b = b.replaceAll("\\(\\)", "");
+            b = b.replaceAll("\\[\\]", "");
+            b = b.replaceAll("\\{\\}", "");
+            if(b.length() == 0)
+                return true;
+        }
+        return false;
     }
 }
--- src/test/java/gababartnicka/dev/SolutionTest.java
+++ src/test/java/gababartnicka/dev/SolutionTest.java
@@ -6,40 +6,24 @@ import static org.junit.Assert.*;
 
 public class SolutionTest {
 
+    private Solution checker = new Solution();
+
     @Test
-    public void testBasic() {
-        assertFalse("0  is not prime", Solution.isPrime(0));
-        assertFalse("1  is not prime", Solution.isPrime(1));
-        assertTrue ("2  is prime",     Solution.isPrime(2));
-        assertTrue ("73 is prime",     Solution.isPrime(73));
-        assertFalse("75 is not prime", Solution.isPrime(75));
-        assertFalse("-1 is not prime", Solution.isPrime(-1));
+    public void testValid() {
+        assertEquals(true, checker.isValid("()"));
     }
 
     @Test
-    public void testPrime() {
-        assertTrue("3 is prime", Solution.isPrime(3));
-        assertTrue("5 is prime", Solution.isPrime(5));
-        assertTrue("7 is prime", Solution.isPrime(7));
-        assertTrue("41 is prime", Solution.isPrime(41));
-        assertTrue("5099 is prime", Solution.isPrime(5099));
+    public void testInvalid() {
+        assertEquals(false, checker.isValid("[(])"));
     }
 
     @Test
-    public void testNotPrime() {
-        assertFalse("4 is not prime", Solution.isPrime(4));
-        assertFalse("6 is not prime", Solution.isPrime(6));
-        assertFalse("8 is not prime", Solution.isPrime(8));
-        assertFalse("9 is not prime", Solution.isPrime(9));
-        assertFalse("45 is not prime", Solution.isPrime(45));
-        assertFalse("-5 is not prime", Solution.isPrime(-5));
-        assertFalse("-8 is not prime", Solution.isPrime(-8));
-        assertFalse("-41 is not prime", Solution.isPrime(-41));
-    }
-
-    @Test
-    public void testRandom() {
-        assertFalse("983261449 is not prime", Solution.isPrime(983261449));
-        assertFalse("334414369 is not prime", Solution.isPrime(334414369));
+    public void test() {
+        assertTrue(checker.isValid("(){}[]"));
+        assertTrue(checker.isValid("([{}])"));
+        assertFalse(checker.isValid("(}"));
+        assertFalse(checker.isValid("[(])"));
+        assertFalse(checker.isValid("[({})](]"));
     }
 }
