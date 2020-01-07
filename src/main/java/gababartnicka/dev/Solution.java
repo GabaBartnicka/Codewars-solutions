2e2b1625bc69c11f44ddbf486f334301506d642f
Gaba Bartnicka <bartnicka.g@gmail.com>
2020-01-07 20:27:28 +0100

    Find the odd int https://www.codewars.com/kata/54da5a58ea159efa38000836

--- src/main/java/gababartnicka/dev/Solution.java
+++ src/main/java/gababartnicka/dev/Solution.java
@@ -1,99 +1,31 @@
 package gababartnicka.dev;
 
-import java.util.ArrayList;
-import java.util.Objects;
-import java.util.Stack;
-import java.util.function.Predicate;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class Solution {
 
-    static final Character openBrace = '(';
-    static final Character openBracket = '[';
-    static final Character openCurlyBrace = '{';
+    public static int findIt(int[] a) {
+        return Arrays.stream(a)
+                .mapToObj(value -> (Integer) value)
+                .collect(Collectors.groupingBy(e -> e, Collectors.counting()))
+                .entrySet()
+                .stream()
+                .filter(integerLongEntry -> integerLongEntry.getValue() % 2 != 0)
+                .findAny()
+                .get()
+                .getKey();
+    }
 
-    static final Character closingBrace = ')';
-    static final Character closingBracket = ']';
-    static final Character closingCurlyBrace = '}';
-
-    public boolean isValid(String braces) {
-
-        Predicate<Character> isClosing = character -> character.equals(closingBrace) ||
-                character.equals(closingBracket) || character.equals(closingCurlyBrace);
-
-        final char[] bracesArray = braces.toCharArray();
-        if (bracesArray.length == 1)
-            return false;
-
-        if (isClosing.test(bracesArray[0]))
-            return false;
-
-        var braceQueue = new ArrayList<Character>();
-        braceQueue.add(bracesArray[0]);
-
-        for (int i = 1; i < bracesArray.length; i++) {
-            final var brace = bracesArray[i];
-
-            if (isClosing.test(brace)) {
-                try {
-                    final int index = braceQueue.size() - 1;
-                    var previous = braceQueue.get(index);
-                    braceQueue.remove(index);
-                    var matchesToPreviousOpenedBrace = matchesToPreviousOpenedBrace(brace, previous);
-                    if (!matchesToPreviousOpenedBrace) {
-                        return false;
-                    }
-                } catch (Exception e) {
-                    return false;
-                }
-            } else {
-                braceQueue.add(brace);
-            }
+    public static int findItXor(int[] A) {
+        int xor = 0;
+        for (int i = 0; i < A.length; i++) {
+            xor ^= A[i];
         }
-        return braceQueue.isEmpty();
+        return xor;
     }
 
-
-    public boolean matchesToPreviousOpenedBrace(Character closing, Character previous) {
-        if (Objects.equals(closing, closingBrace) && Objects.equals(previous, openBrace))
-            return true;
-        if (Objects.equals(closing, closingBracket) && Objects.equals(previous, openBracket))
-            return true;
-        return Objects.equals(closing, closingCurlyBrace) && Objects.equals(previous, openCurlyBrace);
-    }
-
-    /**
-     * "(){}[]"   =>  True
-     * "([{}])"   =>  True
-     * "(}"       =>  False
-     * "[(])"     =>  False
-     * "[({})](]" =>  False
-     */
-
-
-    public boolean isValid2(String braces) {
-        Stack<Character> s = new Stack<>();
-        for (char c : braces.toCharArray())
-            if (s.size() > 0 && isClosing(s.peek(), c)) s.pop();
-            else s.push(c);
-        return s.size() == 0;
-    }
-
-    public boolean isClosing(char x, char c) {
-        return (x == '{' && c == '}') || (x == '(' && c == ')') || (x == '[' && c == ']');
-    }
-
-
-    public boolean isValid3(String braces) {
-        String b = braces;
-        System.out.println(braces);
-        for(int i=0;i<braces.length()/2;i++)
-        {
-            b = b.replaceAll("\\(\\)", "");
-            b = b.replaceAll("\\[\\]", "");
-            b = b.replaceAll("\\{\\}", "");
-            if(b.length() == 0)
-                return true;
-        }
-        return false;
+    public static int findItReduce(int[] arr) {
+        return Arrays.stream(arr).reduce(0, (x, y) -> x ^ y);
     }
 }
--- src/test/java/gababartnicka/dev/SolutionTest.java
+++ src/test/java/gababartnicka/dev/SolutionTest.java
@@ -2,28 +2,23 @@ package gababartnicka.dev;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class SolutionTest {
 
-    private Solution checker = new Solution();
-
     @Test
-    public void testValid() {
-        assertEquals(true, checker.isValid("()"));
-    }
-
-    @Test
-    public void testInvalid() {
-        assertEquals(false, checker.isValid("[(])"));
+    public void findTest() {
+        assertEquals(5, Solution.findIt(new int[]{20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5}));
+        assertEquals(-1, Solution.findIt(new int[]{1, 1, 2, -2, 5, 2, 4, 4, -1, -2, 5}));
+        assertEquals(5, Solution.findIt(new int[]{20, 1, 1, 2, 2, 3, 3, 5, 5, 4, 20, 4, 5}));
+        assertEquals(10, Solution.findIt(new int[]{10}));
+        assertEquals(10, Solution.findIt(new int[]{1, 1, 1, 1, 1, 1, 10, 1, 1, 1, 1}));
+        assertEquals(1, Solution.findIt(new int[]{5, 4, 3, 2, 1, 5, 4, 3, 2, 10, 10}));
     }
 
     @Test
     public void test() {
-        assertTrue(checker.isValid("(){}[]"));
-        assertTrue(checker.isValid("([{}])"));
-        assertFalse(checker.isValid("(}"));
-        assertFalse(checker.isValid("[(])"));
-        assertFalse(checker.isValid("[({})](]"));
+        assertEquals(-1, Solution.findIt(new int[]{1, 1, 2, -2, 5, 2, 4, 4, -1, -2, 5}));
+
     }
 }
