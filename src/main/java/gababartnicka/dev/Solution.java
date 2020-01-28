f8b8e7121d415c1f6819c8533d530b3a2fd0f69b
Gaba Bartnicka <bartnicka.g@gmail.com>
2020-01-28 22:17:12 +0100

    Range Extraction https://www.codewars.com/kata/51ba717bb08c1cd60f00002f/

--- src/main/java/gababartnicka/dev/Solution.java
+++ src/main/java/gababartnicka/dev/Solution.java
@@ -1,46 +1,35 @@
 package gababartnicka.dev;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
 public class Solution {
+    public static String rangeExtraction(int[] arr) {
+        var sb = new StringBuilder();
 
-    public static List<String> top3(String s) {
-        final String regex = "\\s|:|;|_|-|!|\\.|,|/|\\?+";
-        return Arrays.stream(s.split(regex))
-                .collect(Collectors.groupingBy(e -> e, Collectors.counting()))
-                .entrySet()
-                .stream()
-                .sorted((o1, o2) -> o1.getKey().compareTo(o2.getKey()))
-                .sorted((o1, o2) -> o2.getValue().compareTo(o1.getValue()))
-                .map(Map.Entry::getKey)
-                .map(String::toLowerCase)
-                .map(w -> w.replaceAll("[^a-zA-Z']", ""))
-                .filter(w -> !w.isEmpty())
-                .filter(w -> w.matches("^.*[a-zA-Z]+.*$"))
-                .limit(3)
-                .collect(Collectors.toList());
-    }
+        for (int i = 0; i < arr.length; i++) {
+            var element = arr[i];
+            var next = "";
 
-    /*
-    final static private Pattern P = Pattern.compile("[a-z][a-z']*");
+            int k = 1;
+            int n = 0;
+            for (int j = i + 1; j < arr.length; j++, k++) {
+                if (arr[j] != element + k) {
+                    break;
+                } else {
+                    next = String.valueOf(arr[j]);
+                    n++;
+                }
+            }
+            if (n > 1)
+                i = i + (k - 1);
 
-    public static List<String> top3(String s) {
-
-        Map<String,Integer> cnt = new HashMap<>();
-        Matcher m = P.matcher(s.toLowerCase());
-        while (m.find()) {
-            cnt.put(m.group(), cnt.getOrDefault(m.group(), 0)+1);
+            if (next.isEmpty() || n <= 1) {
+                sb.append(element).append(",");
+            } else {
+                sb.append(element).append("-").append(next).append(",");
+            }
         }
-        return cnt.entrySet().stream().sorted(TopWords::compare)
-                  .limit(3).map(Map.Entry::getKey).collect(toList());
-    }
 
-    private static int compare(Map.Entry<String,Integer> a, Map.Entry<String,Integer> b) {
-        int u = a.getValue(), v = b.getValue();
-        return -Integer.compare(u,v);
+        if (sb.charAt(sb.length() - 1) == ',')
+            sb.deleteCharAt(sb.length() - 1);
+        return sb.toString();
     }
-     */
 }
--- src/test/java/gababartnicka/dev/SolutionTest.java
+++ src/test/java/gababartnicka/dev/SolutionTest.java
@@ -2,66 +2,25 @@ package gababartnicka.dev;
 
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
 import static org.junit.Assert.assertEquals;
 
 public class SolutionTest {
+    @Test
+    public void test_BasicTests() {
+        assertEquals("-6,-3-1,3-5,7-11,14,15,17-20", Solution.rangeExtraction(new int[]{-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20}));
+
+        assertEquals("-3--1,2,10,15,16,18-20", Solution.rangeExtraction(new int[]{-3, -2, -1, 2, 10, 15, 16, 18, 19, 20}));
+    }
 
     @Test
-    public void sampleTests() {
-        assertEquals(Arrays.asList("e", "d", "a"), Solution.top3("a a a  b  c c  d d d d  e e e e e"));
-        assertEquals(Arrays.asList("e", "ddd", "aa"), Solution.top3("e e e e DDD ddd DdD: ddd ddd aa aA Aa, bb cc cC e e e"));
-        assertEquals(Arrays.asList("won't", "wont"), Solution.top3("  //wont won't won't "));
-        assertEquals(Arrays.asList("e"), Solution.top3("  , e   .. "));
-        assertEquals(Arrays.asList(), Solution.top3("  ...  "));
-        assertEquals(Arrays.asList(), Solution.top3("  '  "));
-        assertEquals(Arrays.asList(), Solution.top3("  '''  "));
-        assertEquals(Arrays.asList("a", "of", "on"), Solution.top3(Stream
-                .of("In a village of La Mancha, the name of which I have no desire to call to",
-                        "mind, there lived not long since one of those gentlemen that keep a lance",
-                        "in the lance-rack, an old buckler, a lean hack, and a greyhound for",
-                        "coursing. An olla of rather more beef than mutton, a salad on most",
-                        "nights, scraps on Saturdays, lentils on Fridays, and a pigeon or so extra",
-                        "on Sundays, made away with three-quarters of his income.")
-                .collect(Collectors.joining("\n"))));
+    public void failing() {
+        //-11, -9, -8, -6, -3, 0, 1, 3, 5, 7, 10, 11, 13, 14, 17, 19, 22, 24, 27, 28, 30,
+        assertEquals("-11,-9,-8,-6,-3,0,1,3,5,7,10,11,13,14,17,19,22,24,27,28,30",
+                Solution.rangeExtraction(new int[]{-11, -9, -8, -6, -3, 0, 1, 3, 5, 7, 10, 11, 13, 14, 17, 19, 22, 24, 27, 28, 30}));
     }
 
     @Test
     public void single() {
-        assertEquals(Arrays.asList("won't", "wont"), Solution.top3("  //wont won't won't "));
-    }
-
-    @Test
-    public void failingRandom() {
-        assertEquals(Arrays.asList("qbs", "hsytypj", "oatsu"),
-                Solution.top3("qgRpOKpZBp " +
-                        "avanKUkJmh!jbUmOSde_qgRpOKpZBp " +
-                        "jbUmOSde " +
-                        "jbUmOSde.qgRpOKpZBp?avanKUkJmh " +
-                        "jbUmOSde " +
-                        "avanKUkJmh;qgRpOKpZBp " +
-                        "avanKUkJmh " +
-                        "TmaaRj'n " +
-                        "avanKUkJmh?jbUmOSde.TmaaRj'n/avanKUkJmh " +
-                        "avanKUkJmh " +
-                        "jbUmOSde:qgRpOKpZBp;avanKUkJmh:avanKUkJmh " +
-                        "avanKUkJmh_avanKUkJmh " +
-                        "jbUmOSde " +
-                        "qgRpOKpZBp " +
-                        "avanKUkJmh " +
-                        "jbUmOSde " +
-                        "avanKUkJmh_jbUmOSde;avanKUkJmh " +
-                        "jbUmOSde " +
-                        "TmaaRj'n-avanKUkJmh " +
-                        "jbUmOSde " +
-                        "qgRpOKpZBp-jbUmOSde " +
-                        "qgRpOKpZBp " +
-                        "avanKUkJmh " +
-                        "avanKUkJmh " +
-                        "avanKUkJmh?jbUmOSde TmaaRj'n-avanKUkJmh jbUmOSde/jbUmOSde!jbUmOSde avanKUkJmh qgRpOKpZBp qgRpOKpZBp avanKUkJmh/jbUmOSde:qgRpOKpZBp?avanKUkJmh-qgRpOKpZBp_TmaaRj'n avanKUkJmh:jbUmOSde\n" +
-                        "RyAnBrHZr;VQNe XdmVJ HSYTYpJ:Qbs CsAsAwTtUz-ohmld oaTsU.VQNe_Qbs_oaTsU Qbs_HSYTYpJ_EJFCmwOMS-VQNe.EJFCmwOMS:EJFCmwOMS:HSYTYpJ!CsAsAwTtUz uwNnXVfv;EJFCmwOMS CsAsAwTtUz CsAsAwTtUz HSYTYpJ,HSYTYpJ IruFa.HmNzdwtlpN:HSYTYpJ XdmVJ kkzpDnDP;ohmld.EJFCmwOMS-Qbs PwuPOV Gduxmzq CsAsAwTtUz HSYTYpJ.VQNe XdmVJ PwuPOV?Qbs?oaTsU:oaTsU RyAnBrHZr?HmNzdwtlpN,kkzpDnDP Qbs PwuPOV/CsAsAwTtUz VQNe?Qbs-VQNe?HSYTYpJ VQNe_IruFa-oaTsU/oaTsU;HSYTYpJ EJFCmwOMS_oaTsU_CsAsAwTtUz,oaTsU:PwuPOV:RyAnBrHZr/VQNe.oaTsU RyAnBrHZr.EJFCmwOMS PwuPOV PwuPOV RyAnBrHZr kkzpDnDP-kkzpDnDP;IruFa kkzpDnDP kkzpDnDP.Qbs oaTsU-Qbs Qbs WvEiacsA VQNe,oaTsU;HSYTYpJ ITWtGvpPZS oaTsU Qbs HSYTYpJ Qbs Qbs HSYTYpJ/XdmVJ:Gduxmzq VQNe ohmld ohmld?HSYTYpJ HSYTYpJ/yHFkr_Qbs:IruFa,Qbs.VQNe-EJFCmwOMS,oaTsU RyAnBrHZr.HmNzdwtlpN HSYTYpJ PwuPOV oaTsU PwuPOV XdmVJ VQNe/WvEiacsA Qbs/WvEiacsA:Qbs EJFCmwOMS,IruFa,XdmVJ!kkzpDnDP yHFkr XdmVJ XdmVJ PwuPOV:Qbs?oaTsU XdmVJ.XdmVJ oaTsU.Qbs VQNe RyAnBrHZr?IruFa;Qbs;HSYTYpJ-Qbs PwuPOV VQNe PwuPOV bmINGcuV EJFCmwOMS/WvEiacsA kkzpDnDP!HSYTYpJ IruFa HSYTYpJ,VQNe.kkzpDnDP PwuPOV:HmNzdwtlpN uwNnXVfv CsAsAwTtUz VQNe CsAsAwTtUz XdmVJ PwuPOV!oaTsU EJFCmwOMS ohmld:Qbs;IruFa!Qbs;RyAnBrHZr HSYTYpJ PwuPOV.IruFa,VQNe HSYTYpJ IruFa;Qbs EJFCmwOMS kkzpDnDP?XdmVJ?HSYTYpJ oaTsU EJFCmwOMS.PwuPOV,oaTsU_CsAsAwTtUz-HSYTYpJ oaTsU!VQNe_XdmVJ oaTsU:VQNe VQNe?XdmVJ EJFCmwOMS EJFCmwOMS-oaTsU;oaTsU_HSYTYpJ?VQNe VQNe XdmVJ oaTsU Qbs CsAsAwTtUz_HSYTYpJ Qbs-PwuPOV;EJFCmwOMS HSYTYpJ!Qbs Qbs kkzpDnDP?HSYTYpJ;oaTsU VQNe XdmVJ VQNe EJFCmwOMS ohmld kkzpDnDP_HmNzdwtlpN-Qbs XdmVJ XdmVJ-XdmVJ oaTsU"));
+        assertEquals("-6,-3-1,3-5,7-11,14,15,17-20", Solution.rangeExtraction(new int[]{-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20,}));
     }
 }
